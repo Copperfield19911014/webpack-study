4131dad18e852dc7d5d8a6bc825e809677205fd0
cui pengfei <cpf19911014@126.com>
2021-06-09 17:16:13 +0800

    babel插件学习及webpack5新配置用法

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,45 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
     mode: 'development',
-    entry: './index.js',
+    // target: "node",
+    resolve: {
+      alias: {
+        process: "process/browser"
+      },
+      fallback: {
+        "fs": false,
+        // "tls": false,
+        // "net": false,
+        "path": require.resolve('path-browserify'),
+        "assert": require.resolve("assert/"),
+        "process": require.resolve('process')
+        // "buffer": require.resolve("buffer/")
+      } 
+    },
+    entry: './practise.js',
     output: {
         publicPath: './',
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './dist'),
         clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
     },
     devServer: {
         contentBase: './dist'
@@ -16,6 +47,13 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: '管理输出'
-        })
-    ]
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ["buffer", "Buffer"]
+        }),
+    ],
+    // plugins: [
+    //   new NodePolyfillPlugin()
+    // ]
 };
